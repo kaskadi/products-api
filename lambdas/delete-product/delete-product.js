c145d21871c93fe44cac675a62b25c7f683dca59
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-12 17:21:40 +0200

    Using new utility in delete-product

--- lambdas/delete-product/delete-product.js
+++ lambdas/delete-product/delete-product.js
@@ -1,8 +1,6 @@
-const { getBaseResponse, createEsClient } = require('products-api-utils')
+const { getBaseResponse, createEsClient, getId } = require('products-api-utils')
 const deleteEntry = require('./helpers/delete-entry.js')
 
 module.exports.handler = async (event) => {
-  let id = event.pathParameters ? event.pathParameters.id : undefined
-  id = id && id.trim().length > 0 ? id.trim() : undefined
-  return await deleteEntry(createEsClient(), id, getBaseResponse())
+  return await deleteEntry(createEsClient(), getId(event), getBaseResponse())
 }
