df8d0a95d165789193b47099ea39d1ce5d37ead9
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-12 18:41:54 +0200

    Updated ID retrieval logic

--- layer/nodejs/products-api-utils/get-id.js
+++ layer/nodejs/products-api-utils/get-id.js
@@ -1,5 +1,5 @@
 module.exports = event => {
   let id = event.pathParameters ? event.pathParameters.id : undefined
-  id = id && id.trim().length > 0 ? id.trim() : undefined
+  id = id && id.replace(/%20/g, '').replace(/\+/g, '').length > 0 ? id.replace(/%20/g, '').replace(/\+/g, '') : undefined
   return id
 }
