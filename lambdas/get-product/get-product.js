aa4254bb2d446fa6cbd2c0b3be2a98884357f888
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-12 19:25:48 +0200

    Added ID sanitation

--- lambdas/get-product/get-product.js
+++ lambdas/get-product/get-product.js
@@ -2,5 +2,5 @@ const { getBaseResponse, createEsClient } = require('products-api-utils')
 const getEntry = require('./helpers/get.js')
 
 module.exports.handler = async (event) => {
-  return await getEntry(createEsClient(), event.pathParameters.id, getBaseResponse())
+  return await getEntry(createEsClient(), event.pathParameters.id.replace(/%20/g, ' ').replace(/\+/g, ' '), getBaseResponse())
 }
--- lambdas/save-product/save-product.js
+++ lambdas/save-product/save-product.js
@@ -2,5 +2,5 @@ const { getBaseResponse, createEsClient } = require('products-api-utils')
 const saveEntry = require('./helpers/save.js')
 
 module.exports.handler = async (event) => {
-  return await saveEntry(createEsClient(), event.pathParameters.id, JSON.parse(event.body), getBaseResponse())
+  return await saveEntry(createEsClient(), event.pathParameters.id.replace(/%20/g, ' ').replace(/\+/g, ' '), JSON.parse(event.body), getBaseResponse())
 }
