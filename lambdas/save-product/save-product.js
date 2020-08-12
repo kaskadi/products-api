9aa6611f023b55a8c40d1da65bf74d27e3a1e708
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-12 17:59:23 +0200

    Implemented save-product logic

--- lambdas/save-product/save-product.js
+++ lambdas/save-product/save-product.js
@@ -1,11 +1,6 @@
+const { getBaseResponse, createEsClient, getId } = require('products-api-utils')
+const saveEntry = require('./helpers/save.js')
+
 module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      message: 'Hello World!'
-    })
-  }
+  return await saveEntry(createEsClient(), getId(event), JSON.parse(event.body), getBaseResponse())
 }
