5a3a37e740a089ddff3bce2ca23fca5bf8bd3a1a
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-12 17:44:55 +0200

    Reverted change made on return value

--- lambdas/search-products/helpers/process-response.js
+++ lambdas/search-products/helpers/process-response.js
@@ -2,6 +2,6 @@ module.exports = baseResponse => res => {
   console.log(JSON.stringify(res, null, 2))
   return {
     ...baseResponse,
-    body: JSON.stringify(res.body.hits.hits)
+    body: JSON.stringify(res.body)
   }
 }
